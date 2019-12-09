474fdc6448667b6d66b2c94b95d91da9a5d649cd
Sergio Carracedo <info@sergiocarracedo.es>
2019-12-09 08:59:19 +0100

    Build

--- config/index.js
+++ config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/agl-igalia-demo',
+    assetsPublicPath: '/agl-igalia-demo/',
 
     /**
      * Source Maps
